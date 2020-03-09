7e5208addcb7430040f0e453cf3e46a91d75b0ac
NeDonil <gatfild@bk.ru>
2020-03-10 00:16:05 +0300

    Blending. Renderer Init

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -141,6 +141,7 @@ public:
 
 		m_TextureShader.reset(Engine::Shader::Create(textureShaderVertexSrc, textureShaderFragmentSrc));
 		m_Texture = Engine::Texture2D::Create("assets/textures/Checkerboard.png");
+		m_AlsoTexture = Engine::Texture2D::Create("assets/textures/icon.png");
 
 		std::dynamic_pointer_cast<Engine::OpenGLShader>(m_TextureShader)->Bind();
 		std::dynamic_pointer_cast<Engine::OpenGLShader>(m_TextureShader)->UploadUniformInt("u_Texture", 0);
@@ -183,6 +184,9 @@ public:
 		m_Texture->Bind();
 		Engine::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
 
+		m_AlsoTexture->Bind();
+		Engine::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(0.33f)));
+
 		Engine::Renderer::EndScene();
 	}
 
@@ -206,7 +210,7 @@ private:
 	Engine::Ref<Engine::Shader> m_FlatColorShader, m_TextureShader;
 	Engine::Ref<Engine::VertexArray> m_SquareVA;
 
-	Engine::Ref<Engine::Texture2D> m_Texture;
+	Engine::Ref<Engine::Texture2D> m_Texture, m_AlsoTexture;
 
 	Engine::OrthographicCamera m_Camera;
 	glm::vec3 m_CameraPosition;
