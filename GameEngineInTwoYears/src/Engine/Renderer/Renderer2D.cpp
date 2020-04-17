b8386e51099a53f1846cd1d1f13cc212b9ebb92a
NeDonil <gatfild@bk.ru>
2020-04-18 02:38:32 +0300

    Renderer Statistics

--- GameEngineInTwoYears/src/Engine/Renderer/Renderer2D.cpp
+++ GameEngineInTwoYears/src/Engine/Renderer/Renderer2D.cpp
@@ -19,9 +19,9 @@ namespace Engine
 
 	struct Renderer2DData
 	{
-		const uint32_t MaxQuads = 10000;
-		const uint32_t MaxVertices = MaxQuads * 4;
-		const uint32_t MaxIndices = MaxQuads * 6;
+		static const uint32_t MaxQuads = 20000;
+		static const uint32_t MaxVertices = MaxQuads * 4;
+		static const uint32_t MaxIndices = MaxQuads * 6;
 		static const uint32_t MaxTextureSlots = 32;
 
 		Ref<VertexArray> QuadVertexArray;
@@ -37,8 +37,9 @@ namespace Engine
 		uint32_t TextureSlotIndex = 1;
 
 		glm::vec4 QuadVertices[4];
-	};
 
+		Renderer2D::Statistics Stats;
+	};
 
 	static Renderer2DData s_Data;
 
@@ -129,6 +130,17 @@ namespace Engine
 		}
 
 		RenderCommand::DrawIndexed(s_Data.QuadVertexArray, s_Data.QuadIndexCount);
+		s_Data.Stats.DrawCalls++;
+	}
+
+	void Renderer2D::FlushAndReset()
+	{
+		EndScene();
+
+		s_Data.QuadIndexCount = 0;
+		s_Data.QuadVertexBufferPtr = s_Data.QuadVertexBufferBase;
+
+		s_Data.TextureSlotIndex = 1;
 	}
 
 	void Renderer2D::DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color)
@@ -138,6 +150,9 @@ namespace Engine
 
 	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color)
 	{
+		if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+			FlushAndReset();
+
 		const float texIndex = 0.0f;
 		const float tilingFactor = 1.0f;
 
@@ -170,6 +185,8 @@ namespace Engine
 		s_Data.QuadVertexBufferPtr++;
 
 		s_Data.QuadIndexCount += 6;
+
+		s_Data.Stats.QuadCount++;
 	}
 
 	void Renderer2D::DrawQuad(const glm::vec2& position, const glm::vec2& size, const Ref<Texture2D>& texture, float tilingFactor)
@@ -179,6 +196,9 @@ namespace Engine
 
 	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const Ref<Texture2D>& texture, float tilingFactor)
 	{
+		if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+			FlushAndReset();
+
 		constexpr glm::vec4 color = { 1.0f, 1.0f, 1.0f, 1.0f };
 
 		float textureIndex = 0.0f;
@@ -227,6 +247,8 @@ namespace Engine
 		s_Data.QuadVertexBufferPtr++;
 
 		s_Data.QuadIndexCount += 6;
+
+		s_Data.Stats.QuadCount++;
 	}
 
 	void Renderer2D::DrawRotatedQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const glm::vec4& color)
@@ -236,6 +258,9 @@ namespace Engine
 
 	void Renderer2D::DrawRotatedQuad(const glm::vec3& position, const glm::vec2& size, float rotation, const glm::vec4& color)
 	{
+		if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+			FlushAndReset();
+
 		const float texIndex = 0.0f;
 		const float tilingFactor = 1.0f;
 
@@ -273,6 +298,8 @@ namespace Engine
 		s_Data.QuadVertexBufferPtr++;
 
 		s_Data.QuadIndexCount += 6;
+
+		s_Data.Stats.QuadCount++;
 	}
 
 	void Renderer2D::DrawRotatedQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const Ref<Texture2D>& texture, float tilingFactor)
@@ -282,6 +309,9 @@ namespace Engine
 
 	void Renderer2D::DrawRotatedQuad(const glm::vec3& position, const glm::vec2& size, float rotation, const Ref<Texture2D>& texture, float tilingFactor)
 	{
+		if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+			FlushAndReset();
+
 		constexpr glm::vec4 color = { 1.0f, 1.0f, 1.0f, 1.0f };
 
 		float textureIndex = 0.0f;
@@ -333,5 +363,17 @@ namespace Engine
 		s_Data.QuadVertexBufferPtr->TexIndex = textureIndex;
 		s_Data.QuadVertexBufferPtr->TilingFactor = tilingFactor;
 		s_Data.QuadVertexBufferPtr++;
+
+		s_Data.Stats.QuadCount++;
+	}
+
+	Renderer2D::Statistics Renderer2D::GetStats()
+	{
+		return s_Data.Stats;
+	}
+
+	void Renderer2D::ResetStats()
+	{
+		memset(&s_Data.Stats, 0, sizeof(Statistics));
 	}
 }
--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -30,6 +30,7 @@ void Sandbox2D::OnUpdate(Engine::Timestep ts)
 		m_CameraController.OnUpdate(ts);
 	}
 
+	Engine::Renderer2D::ResetStats();
 	{
 		ENGINE_PROFILE_SCOPE("Sandbox2D::Renderer prep");
 		Engine::RenderCommand::SetClearColor({ 0.2f, 0.2f, 0.2f, 1.0f });
@@ -40,24 +41,33 @@ void Sandbox2D::OnUpdate(Engine::Timestep ts)
 		ENGINE_PROFILE_SCOPE("Sandbox2D::Renderer draw");
 		Engine::Renderer2D::BeginScene(m_CameraController.GetCamera());
 
-		Engine::Renderer2D::DrawQuad({ -3, -3, -0.1f }, { 10.0f, 10.0f}, m_CheckerboardTexture, 20.0f);
+		Engine::Renderer2D::DrawQuad({ 0, 0, -0.1f }, { 20.0f, 20.0f}, m_CheckerboardTexture, 20.0f);
 
-		Engine::Renderer2D::DrawQuad({ 1.5f,  -1.0f, 0.2f }, { 5.0, 0.25 }, m_RedColor);
-		Engine::Renderer2D::DrawQuad({ 1.5f,  -1.25f, 0.2f}, { 5.0, 0.25 }, m_OrangeColor);
-		Engine::Renderer2D::DrawQuad({ 1.5f,  -1.50f, 0.2f}, { 5.0, 0.25 }, m_YellowColor);
-		Engine::Renderer2D::DrawQuad({ 1.5f,  -1.75f, 0.2f}, { 5.0, 0.25 }, m_GreenColor);
-		Engine::Renderer2D::DrawQuad({ 1.5f,  -2.0f , 0.2f}, { 5.0, 0.25 }, m_BlueColor);
-		Engine::Renderer2D::DrawQuad({ 1.5f,  -2.25f, 0.2f}, { 5.0, 0.25 }, m_PurpleColor);
+		//Engine::Renderer2D::DrawQuad({ 1.5f,  -1.0f, 0.2f }, { 5.0, 0.25 }, m_RedColor);
+		//Engine::Renderer2D::DrawQuad({ 1.5f,  -1.25f, 0.2f}, { 5.0, 0.25 }, m_OrangeColor);
+		//Engine::Renderer2D::DrawQuad({ 1.5f,  -1.50f, 0.2f}, { 5.0, 0.25 }, m_YellowColor);
+		//Engine::Renderer2D::DrawQuad({ 1.5f,  -1.75f, 0.2f}, { 5.0, 0.25 }, m_GreenColor);
+		//Engine::Renderer2D::DrawQuad({ 1.5f,  -2.0f , 0.2f}, { 5.0, 0.25 }, m_BlueColor);
+		//Engine::Renderer2D::DrawQuad({ 1.5f,  -2.25f, 0.2f}, { 5.0, 0.25 }, m_PurpleColor);
 
-		Engine::Renderer2D::DrawRotatedQuad({ 0, 0}, { 1.0f, 1.0f }, 30.0f, m_SquareColor);
-		Engine::Renderer2D::DrawRotatedQuad({ 4, 0}, { 1.0f, 1.0f }, 45.0f, m_SquareColor);
-		Engine::Renderer2D::DrawRotatedQuad({ 8, 0}, { 1.0f, 1.0f }, 60.0f, m_SquareColor);
+		for (float y = 0.0f; y < 20.0f; y += 0.5f)
+		{
+			for (float x = 0.0f; x < 20.0f; x += 0.5f)
+			{
+				glm::vec4 color = { (x + 5.0f) / 10.0f, 0.4f, (y + 5.0f) / 10.0f, 0.5f };
+				Engine::Renderer2D::DrawQuad({ x,  y, 0.05f }, { 0.45, 0.45 }, color);
+			}
+		}
 
-		Engine::Renderer2D::DrawQuad({ 0.0f,  0.0f, 0.1f }, { 0.5, 0.5 }, m_Texture);
-		Engine::Renderer2D::DrawRotatedQuad({ 1.0f,  0.0f, 0.1f }, { 0.5, 0.5 }, 30.0f, m_Texture);
-		Engine::Renderer2D::DrawRotatedQuad({ 2.0f,  0.0f, 0.1f }, { 0.5, 0.5 }, 45.0f, m_Texture);
-		Engine::Renderer2D::DrawRotatedQuad({ 3.0f,  0.0f, 0.1f }, { 0.5, 0.5 }, 60.0f, m_Texture);
-		Engine::Renderer2D::DrawRotatedQuad({ 4.0f,  0.0f, 0.1f }, { 0.5, 0.5 }, 90.0f, m_Texture);
+		Engine::Renderer2D::DrawRotatedQuad({ 10, 3, 0.1}, { 1.0f, 1.0f }, 30.0f, m_SquareColor);
+		Engine::Renderer2D::DrawRotatedQuad({ 14, 3, 0.1}, { 1.0f, 1.0f }, 45.0f, m_SquareColor);
+		Engine::Renderer2D::DrawRotatedQuad({ 18, 3, 0.1}, { 1.0f, 1.0f }, 60.0f, m_SquareColor);
+
+		Engine::Renderer2D::DrawQuad(       { 4.0f,  5.0f, 0.15f }, { 0.5, 0.5 }, m_Texture);
+		Engine::Renderer2D::DrawRotatedQuad({ 5.0f,  5.0f, 0.15f }, { 0.5, 0.5 }, 30.0f, m_Texture);
+		Engine::Renderer2D::DrawRotatedQuad({ 6.0f,  5.0f, 0.15f }, { 0.5, 0.5 }, 45.0f, m_Texture);
+		Engine::Renderer2D::DrawRotatedQuad({ 7.0f,  5.0f, 0.15f }, { 0.5, 0.5 }, 60.0f, m_Texture);
+		Engine::Renderer2D::DrawRotatedQuad({ 8.0f,  5.0f, 0.15f }, { 0.5, 0.5 }, 90.0f, m_Texture);
 
 		Engine::Renderer2D::EndScene();
 	}
@@ -68,13 +78,14 @@ void Sandbox2D::OnImGuiRender()
 	ENGINE_PROFILE_FUNCTION();
 	ImGui::Begin("Square");
 
+	auto stats = Engine::Renderer2D::GetStats();
+	ImGui::Text("Renderer2D Stats:");
+	ImGui::Text("Draw Calls: %d", stats.DrawCalls);
+	ImGui::Text("Quad Count: %d", stats.QuadCount);
+	ImGui::Text("Total Vertex Count: %d", stats.GetTotalVertexCount());
+	ImGui::Text("Total Index Count: %d", stats.GetTotalIndexCount());
+
 	ImGui::ColorEdit4("Square color", glm::value_ptr(m_SquareColor));
-	ImGui::ColorEdit4("Red color", glm::value_ptr(m_RedColor));
-	ImGui::ColorEdit4("Orange color", glm::value_ptr(m_OrangeColor));
-	ImGui::ColorEdit4("Yellow color", glm::value_ptr(m_YellowColor));
-	ImGui::ColorEdit4("Green color", glm::value_ptr(m_GreenColor));
-	ImGui::ColorEdit4("Blue color", glm::value_ptr(m_BlueColor));
-	ImGui::ColorEdit4("Square Purple", glm::value_ptr(m_PurpleColor));
 
 	ImGui::End();
 }
